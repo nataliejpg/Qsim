20dfa711c3859e1992cf007e6ee6f654381966f9
nataliejpg <npearson@phys.ethz.ch>
2018-04-27 15:10:07 +0200

    make entropy measurement be log not log2 in mps methods

--- qsim/mps/methods.py
+++ qsim/mps/methods.py
@@ -84,7 +84,7 @@ def find_entropy(mps, k=None):
     if k is None:
         k = int(np.floor(len(mps) / 2))
     mps, sing_vals = normalise_mps(mps, direction='S', k=k)
-    entropy = -1 * sum([i**2 * np.log2(i**2) for i in sing_vals if abs(i) > 0])
+    entropy = -1 * sum([i**2 * np.log(i**2) for i in sing_vals if abs(i) > 0])
     return entropy
 
 
